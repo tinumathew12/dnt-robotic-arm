220701151c7f96a2ac6825a6befeb1e98141c88f
tinumathew12 <96045820+tinumathew12@users.noreply.github.com>
2024-09-07 13:32:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,63 @@ import cv2
 import mediapipe as mp
 import time
 import math
+from pyfirmata import Arduino, SERVO
+import inspect
+
+if not hasattr(inspect, 'getargspec'):
+    inspect.getargspec = inspect.getfullargspec
+
+port='COM7'
+pin=10
+board=Arduino(port)
+board.digital[pin].mode=SERVO
+board.digital[9].mode=SERVO
+board.digital[11].mode=SERVO
 
 
+def rotate(pin,angle):
+    board.digital[pin].write(angle)
+
+'''
+for i in range(180):
+    rotate(9,i)
+    print(i)
+    time.sleep(0.1)
+'''
+
+def bootsequence():
+    rotate(11,90)
+
+    for i in range(170,90,-1):
+        rotate(10,i)
+        time.sleep(0.1)
+    for m in range(20,0,-1):
+        rotate(9,m)
+        time.sleep(0.1)
+    rotate(11,90)
+
+
+bootsequence()  
 
 cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(max_num_hands= 1, min_detection_confidence= 0.5, min_tracking_confidence= 0.5)
+hands = mpHands.Hands(max_num_hands= 1, min_detection_confidence= 0.9, min_tracking_confidence= 0.9)
 mpDraw = mp.solutions.drawing_utils
 
 pTime = 0
 cTime = 0
+angle1=0
+angle2=0
+angle3=0
+angle4=0
+angleyolo=0
+#rotate(10,90)
+#rotate(11,90)
 
 
+finalangle1=20
+finalangle2=0
 def anglefinder(C,A=50,B=50):
     cos_a=((-(A**2))+(B**2)+(C**2))/(2*B*C)
     cos_b=((A**2)+(-(B**2))+(C**2))/(2*A*C)
@@ -32,7 +76,10 @@ def anglecorrector(y,angle,maxangle=90,range=400):
         #y=y*-1
     return((y/range)*maxangle)+angle
 
-
+def bruhh(x):
+    y=(((100-x)/60)*90)+125
+    #print(int(y))
+    return int(y)
 def planeanglefinder(pointA,pointB,pointC):
     x1,y1,z1=pointA[0],pointA[1],pointA[2]
     x2,y2,z2=pointB[0],pointB[1],pointB[2]
@@ -57,7 +104,9 @@ def planeanglefinder(pointA,pointB,pointC):
     if angle<0:
         angle=angle*-1
     return int(angle)
-
+def anglesmoother(x,y,z):
+    for i in range(x,y):
+        rotate(z,i)
 
 
 
@@ -92,7 +141,7 @@ while True:
                 if id==9:
                     coordinate=[cx,cy,cz]
                     yolo=anglefinder((distcalc(coordinate,initcordinate))/5)
-                    print(int(yolo[0]),int(anglecorrector(cy,yolo[1])),(int(anglecorrector(cx,0,90,300)))+90)
+                    #print(int(yolo[0]),int(anglecorrector(cy,yolo[1])),(int(anglecorrector(cx,0,90,300)))+90)
                     angle1=int(yolo[0])
                     angle2=int(anglecorrector(cy,yolo[1]))
                     angle3=(int(anglecorrector(cx,0,90,300)))+90
@@ -104,7 +153,30 @@ while True:
                     pointC=[cx,cy,int(cz)]
                 #print(pointA,pointB,pointC)
                 angle4=planeanglefinder(pointA,pointB,pointC)
-                print(angle1,angle2,angle3,angle4)
+                #print(angle1,angle2,angle3,angle4)
+                if type(angle1) is list:
+                    angle1=25
+                if type(angle2) is list:
+                    angle2=120
+                if type(angle3) is list:
+                    angle3=0
+                if type(angle4) is list:
+                    angle4=0 
+
+
+                #anglesmoother(angleyolo,angle2,10)
+
+                rotate(11,angle3)
+                finalangle2=bruhh(angle2)
+                finalangle1=angle1-15
+                if finalangle1<=0:
+                    finalangle1=0
+                rotate(10,finalangle2)
+                rotate(9,finalangle1)
+
+                print(finalangle1,finalangle2,angle3,angle4)
+
+                #rotate(7,angle4)
 
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
